e22550fcb48c2855dbc2f2280c7acf268f192c69
mahendrajat1996 <msnain9928@gmail>
2023-08-19 23:02:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { useEffect,useState } from 'react';
 import './App.css';
 
 function App() {
+   
+  const [user, setUser] = useState([]);
+
+  const fetchData = () => {
+    return fetch("https://jsonplaceholder.typicode.com/users")
+          .then((response) => response.json())
+          .then((data) => setUser(data));
+  }
+
+  useEffect(() => {
+    fetchData();
+  },[])
+
+  
+
   return (
+    
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='heding'>
+           <p className='welcome'>Welcome back! 👋</p>
+            <h2>Sign in to your account</h2> </div>
+
+      <div className='email'>
+          <p>Your email</p>
+          <input type='text' className='input1' /> </div>
+      <div className='password'>
+          <p>Password</p>
+          <input type='text' className='input2' />
+          </div>
+      <div className='btn1'>
+          <button className='btn' onClick={fetchData}> <b> CONTINUE </b></button> </div>
+
+      <div className='forget'> <a href='' >Forget your password</a> </div>
+
+      
+      
     </div>
+
+    <div className='App2'>Don't have any account<a href='#' >  Sign up</a></div>
+    </>
   );
 }
 
